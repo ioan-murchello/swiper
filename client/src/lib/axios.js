88f0c0929d71b33d29abfd33dea3eb3ab85fee03
ioan-murchello <mihalkurz@gmail.com>
2026-02-10 15:34:53 +0100

    closer closer....

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL + "/api";
+    : import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
   baseURL: BASE_URL,
