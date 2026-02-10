a82dda0cd8dbe7f71548d3082294d78558f109d0
ioan-murchello <mihalkurz@gmail.com>
2026-02-10 15:18:13 +0100

    patheth...

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,11 +1,14 @@
-import axios from 'axios';  
+import axios from "axios";
 
-const BASE_URL= import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : "https://zingy-torte-43b057.netlify.app/";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_API_URL + "/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   withCredentials: true,
 });
